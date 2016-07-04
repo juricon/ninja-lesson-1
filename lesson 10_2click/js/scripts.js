8d3663612ac1a9e4acedc297fa412960f3d02126
juricon <yuriyvladim@gmail.com>
2016-07-05 01:45:16 +0300

    some interesting  stuff

---
+++ lesson 10_2click/js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+/*    $('.btn-for-father').click(function(){
+        $('.father-block').hide('slow');
+    });*/
+/*    $('.btn-for-father').click(function(){
+        $('.father-block').slideUp();
+    });*/
+    $('.btn-for-father').click(function(){
+        $('.father-block').slideUp(2000);
+    });
+});
+
