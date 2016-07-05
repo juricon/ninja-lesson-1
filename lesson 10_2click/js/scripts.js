938327826116e9083e4856e558fac7c82f045fb1
juricon <yuriyvladim@gmail.com>
2016-07-06 01:17:06 +0300

    usual animation in jq

--- lesson 10_2click/js/scripts.js
+++ lesson 10_2click/js/scripts.js
@@ -2,11 +2,32 @@ $(document).ready(function(){
 /*    $('.btn-for-father').click(function(){
         $('.father-block').hide('slow');
     });*/
+/*$('.btn-for-father').click(function(){
+     $('.father-block').show(400);
+     });*/
 /*    $('.btn-for-father').click(function(){
         $('.father-block').slideUp();
     });*/
-    $('.btn-for-father').click(function(){
-        $('.father-block').slideUp(2000);
-    });
+    /*$('.btn-for-father').click(function(){
+        $('.father-block').slideUp(1500);
+     });*/
+/*    $('.btn-for-father').click(function(){
+     $('.father-block').slideUp(5000);
+        });*/
+    /*    $('.btn-for-father').click(function(){
+     $('.father-block').slideUp(5000);
+     });*/
+  /*  $('.btn-for-father').click(function() {
+        $('.father-block').slideToggle(900);
+    }) *//*вроде бы же говорили что эта анимация
+    не работает уже но  заработало. интересная
+    особенность у этой анимации -  работает в две
+     стороны.*/
+  /*  $('.btn-for-father').click(function() {
+     $('.father-block').fadeOut(1000)
+     })*/
+     /*   $('.btn-for-father').click(function() {
+     $('.father-block').fadeIn(1000)
+     });*/
 });
 
