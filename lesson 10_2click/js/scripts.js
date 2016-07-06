e0da62b6dc479e39a1ea21aae6166c46c76b5f51
juricon <yuriyvladim@gmail.com>
2016-07-06 03:19:48 +0300

    simple anim jq  examples

--- lesson 10_2click/js/scripts.js
+++ lesson 10_2click/js/scripts.js
@@ -1,33 +1,41 @@
 $(document).ready(function(){
-/*    $('.btn-for-father').click(function(){
+    $('.btn-for-father').click(function(){
         $('.father-block').hide('slow');
-    });*/
-/*$('.btn-for-father').click(function(){
+    });
+$('.btn-for-father').click(function(){
      $('.father-block').show(400);
-     });*/
-/*    $('.btn-for-father').click(function(){
+     });
+    $('.btn-for-father-slideUp').click(function(){
         $('.father-block').slideUp();
-    });*/
-    /*$('.btn-for-father').click(function(){
+    });
+    $('.btn-for-father').click(function(){
         $('.father-block').slideUp(1500);
-     });*/
-/*    $('.btn-for-father').click(function(){
+     });
+    $('.btn-for-father').click(function(){
      $('.father-block').slideUp(5000);
-        });*/
-    /*    $('.btn-for-father').click(function(){
+        });
+        $('.btn-for-father').click(function(){
      $('.father-block').slideUp(5000);
-     });*/
-  /*  $('.btn-for-father').click(function() {
+     });
+    $('.btn-for-father').click(function() {
         $('.father-block').slideToggle(900);
-    }) *//*вроде бы же говорили что эта анимация
+    })
+    /* вроде бы же говорили что эта анимация
     не работает уже но  заработало. интересная
     особенность у этой анимации -  работает в две
      стороны.*/
-  /*  $('.btn-for-father').click(function() {
+    $('.btn-for-father').click(function() {
      $('.father-block').fadeOut(1000)
-     })*/
-     /*   $('.btn-for-father').click(function() {
+     })
+        $('.btn-for-father').click(function() {
      $('.father-block').fadeIn(1000)
-     });*/
+     });
+       $('.btn-for-father').click(function() {
+     $('.father-block').fadeTo(1000, 0.5)
+     });
+    $('.btn-for-father').click(function() {
+        $('.father-block').fadeToggle(1000)
+    });
+
 });
 
